12b09aaaa2be2476df8a7690d286416f74bd0ad5
Matheus Alves <malves224@gmail.com>
2021-09-11 15:57:40 -0300

    adiciona funçao no button para adicionar uma img a cada click

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,36 +4,54 @@ class Body extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      doguinhoUrl: 'carregando...',
+      loading: true,
+      doguinhoUrl: undefined,
+      urlDogs: [],
     };
+
+    this.addDog = this.addDog.bind(this);
   }
 
   componentDidMount() {
-    this.fetchDoguinho('https://dog.ceo/api/breeds/image/random');
+    this.fetchDoguinho();
   }
 
-  async fetchDoguinho(urlForFetch) {
-    const fetchDog = await fetch(urlForFetch);
-    const fetchDogJson = await fetchDog.json();
+  addDog() {
+    this.setState(({ urlDogs, doguinhoUrl }) => ({
+      urlDogs: [...urlDogs, doguinhoUrl], // urlDogs e o array que será adicionado um elemento, e o doguinhoUrl é o elemento que sera adicionado no array.
+    }));
+    this.fetchDoguinho();// atualiza o state com um novo cachorro.
+  }
+
+  async fetchDoguinho() {
     this.setState({
-      doguinhoUrl: fetchDogJson.message,
+      loading: true,
+    }, async () => {
+      const urlForFetch = 'https://dog.ceo/api/breeds/image/random';
+      const fetchDog = await fetch(urlForFetch);
+      const fetchDogJson = await fetchDog.json();
+      this.setState({
+        loading: false,
+        doguinhoUrl: fetchDogJson.message,
+      });
     });
-    localStorage.setItem('lastUrlDog', fetchDogJson.message);
   }
 
   render() {
-    const { doguinhoUrl } = this.state;
-    const urlForFetch = 'https://dog.ceo/api/breeds/image/random';
+    const { doguinhoUrl, loading, urlDogs} = this.state;
+    const imgDogElement = <img src={ doguinhoUrl } alt="" />;
+    const loadingElement = <p> Loading... </p>;
     return (
       <section className="section">
         <button
-          onClick={ () => this.fetchDoguinho(urlForFetch) }
+          onClick={ this.addDog }
           type="button"
         >
           Change dóginho
         </button>
         <div className="card-dog">
-          <img src={ doguinhoUrl } alt="" />
+          { urlDogs.map((dogUrl) => <img key={ dogUrl } src={ dogUrl } alt="" />)}
+          { loading ? loadingElement : imgDogElement }
         </div>
       </section>
     );
